328fd5be185c7de0babc67738f25679de8c0882f
ross <broderross@gmail.com>
2017-12-06 23:19:36 -0600

    add some breakpoints

--- js/animate.js
+++ js/animate.js
@@ -2,5 +2,5 @@
 
 
 $('.drawer-btn-bg').on('click', function() {
-	$('.nav-full, .logo-bg, .drawer-btn-bg, .links-list, .social-icons-container').toggleClass('open');
+	$('.nav-full, .logo, .drawer-btn-bg, .links-list, .social-icons-container').toggleClass('open');
 });
