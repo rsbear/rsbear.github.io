79fe63cc6f7662a844e29274fb93e4cd897071ed
ross <broderross@gmail.com>
2017-12-14 23:56:01 -0600

    adding social links to main nav

--- js/animate.js
+++ js/animate.js
@@ -8,10 +8,10 @@
 
 
 $('.drawer-btn').on('click', function(){
-    if($('.linking1, .linking2, .linking3').hasClass('NavClose')){
-        $('.linking1, .linking2, .linking3').removeClass('NavClose').addClass('NavOpen');
+    if($('.linking1, .linking2, .linking3, .linking4').hasClass('NavClose')){
+        $('.linking1, .linking2, .linking3, .linking4').removeClass('NavClose').addClass('NavOpen');
     }else{
-        $('.linking1, .linking2, .linking3').addClass('NavClose').removeClass('NavOpen');
+        $('.linking1, .linking2, .linking3, .linking4').addClass('NavClose').removeClass('NavOpen');
     }
 }); 
 
