b14db6f6a2ef37ac426116dc7e2debb4fe020cec
ross <broderross@gmail.com>
2017-12-20 05:09:50 -0600

    pug and sass <3

---
+++ js/animate.js
@@ -0,0 +1,17 @@
+
+$('.nav-mobile-btn').click(function() {
+	$('.footer-mask, .nav-wrapper, .main-nav, .links').toggleClass('NavOpen');
+});
+
+
+$(window).scroll(function () {
+    $('.mid-title, .flex-grid-wrapper').each(function () {
+        var topOfWindow = $(window).scrollTop();
+
+        if (topOfWindow) {
+            $(this).addClass("FadingIn");
+        } else {
+            $(this).removeClass("FadingIn");
+        }
+    });
+});
